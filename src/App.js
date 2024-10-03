80e9c13e6a964aff0c211f2e07895eb975f330e9
Sean Evans <59663233+seanevans-test@users.noreply.github.com>
2024-10-03 12:13:22 +0100

    added a setSquares function for next click to alternate

--- src/App.js
+++ src/App.js
@@ -10,6 +10,13 @@ function Square({ value, onSquareClick }) {
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
+
   return (<>
     <div className="board-row">
       <Square value={squares[0]} onSquareClick={handleClick} />
